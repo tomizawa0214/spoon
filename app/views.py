2b13514769e6bf878e209b807d322835b3f20618
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-05 18:43:42 +0900

    フォームテスト

--- app/views.py
+++ app/views.py
@@ -22,44 +22,43 @@ class OrderView(View):
             size = form.cleaned_data['size']
             flavor = form.cleaned_data['flavor']
             option = form.cleaned_data['option']
-            return render(request, 'app/order_confirm.html', {'form': form})
+            return render(request, 'app/order_confirm.html', {
+                'form': form
+            })
+            
+class OrderConfirmView(View):
+    def post(self, request, *args, **kwargs):
+        size = form.cleaned_data['size']
+        flavor = form.cleaned_data['flavor']
+        option = form.cleaned_data['option']
+        subject = 'ご注文ありがとうございます。'
+        content = textwrap.dedent('''
+            ■サイズ
+            {size}
+            
+            ■フレーバー
+            {flavor}
+            
+            ■オプション
+            {option}
+            --------------------
+            ''').format(
+                size=size,
+                flavor=flavor,
+                option=option
+            )
 
-            if request.POST.get('next', '') == 'back':
-                return render(request, 'app/order.html', {'form': form})
-            if self.request.POST.get('next', '') == 'create':
-                size = form.cleaned_data['size']
-                flavor = form.cleaned_data['flavor']
-                option = form.cleaned_data['option']
-                subject = 'ご注文ありがとうございます。'
-                content = textwrap.dedent('''
-                    ■サイズ
-                    {size}
-                    
-                    ■フレーバー
-                    {flavor}
-                    
-                    ■オプション
-                    {option}
-                    --------------------
-                    ''').format(
-                        size=size,
-                        flavor=flavor,
-                        option=option
-                    )
+        # to_list = [email]
+        bcc_list = [settings.EMAIL_HOST_USER]
 
-                # to_list = [email]
-                bcc_list = [settings.EMAIL_HOST_USER]
+        try:
+            # message = EmailMessage(subject=subject, body=content, to=to_list, bcc=bcc_list)
+            message = EmailMessage(subject=subject, body=content, bcc=bcc_list)
+            message.send()
+        except BadHeaderError:
+            return HttpResponse("無効なヘッダが検出されました。")
 
-                try:
-                    # message = EmailMessage(subject=subject, body=content, to=to_list, bcc=bcc_list)
-                    message = EmailMessage(subject=subject, body=content, bcc=bcc_list)
-                    message.send()
-                except BadHeaderError:
-                    return HttpResponse("無効なヘッダが検出されました。")
-
-                return redirect('index')
-        
-        return render(request, 'app/order.html', {'form': form})
+        return redirect('index')
     
 class ItemDetailView(DetailView):
     model = Item
