d9c63762b6d81aaba15daba480be518024cb5b5c
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-07-08 01:59:37 +0900

    受取時間の計算修正

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from datetime import datetime, date, timedelta
 import datetime
 
+
 # 現在の月を取得
 month = date.today().month
 # 現在の日を取得
@@ -15,69 +16,37 @@ hour = datetime.datetime.now().hour
 # 現在の分を取得
 minute = datetime.datetime.now().minute
 
+
 DATE_CHOICES = []
-# 現在時刻が00:00～17:29の場合
-if hour <  17 or ( hour == 17 and minute < 30 ):
-    # 本日から1週間分を取得
-    days = [date.today() + timedelta(days=day) for day in range(7)]
+def get_weeks(start_day):
     for i in range(7):
         if i == 0:
             DATE_CHOICES.append(
-                (str(days[i].month) + "月" + str(days[i].day) + "日" + "(" + str(flag[days[i].weekday()]) + ")" + "【本日】",
-                str(days[i].month) + "月" + str(days[i].day) + "日" + "(" + str(flag[days[i].weekday()]) + ")" + "【本日】")
+                (str(days[i].month) + "月" + str(days[i].day) + "日" + (str(flag[days[i].weekday()])) + start_day,
+                str(days[i].month) + "月" + str(days[i].day) + "日" + (str(flag[days[i].weekday()])) + start_day)
             )
         else:
             DATE_CHOICES.append(
-                (str(days[i].month) + "月" + str(days[i].day) + "日" + "(" + str(flag[days[i].weekday()]) + ")",
-                str(days[i].month) + "月" + str(days[i].day) + "日" + "(" + str(flag[days[i].weekday()]) + ")")
-            )
-# 現在時刻が17:30以降の場合
-elif hour > 17 or ( hour == 17 and minute > 29 ):
-    # 翌日から1週間分を取得
-    days = [date.today() + timedelta(days=day) for day in range(8)]
-    for i in range(1, 8):
-        if i == 1:
-            DATE_CHOICES.append(
-                (str(days[i].month) + "月" + str(days[i].day) + "日" + "(" + str(flag[days[i].weekday()]) + ")" + "【明日】",
-                str(days[i].month) + "月" + str(days[i].day) + "日" + "(" + str(flag[days[i].weekday()]) + ")" + "【明日】")
-            )
-        else:
-            DATE_CHOICES.append(
-                (str(days[i].month) + "月" + str(days[i].day) + "日" + "(" + str(flag[days[i].weekday()]) + ")",
-                str(days[i].month) + "月" + str(days[i].day) + "日" + "(" + str(flag[days[i].weekday()]) + ")")
+                (str(days[i].month) + "月" + str(days[i].day) + "日" + (str(flag[days[i].weekday()])),
+                str(days[i].month) + "月" + str(days[i].day) + "日" + (str(flag[days[i].weekday()])))
             )
 
-TIME_CHOICES = []
-# 現在時刻が00:00～17:29の場合
+# 現在時刻が00:00～17:29
 if hour <  17 or ( hour == 17 and minute < 30 ):
-    # 現在時刻から30分後
-    today_30 = datetime.timedelta.now() + datetime.timedelta(minute=30)
-    # 30分後の分単位が30より小さい場合
-    if today_30.minute < 30:
-        for j in range(today_30.hour, 19):
-            if j != today_30.hour:
-                TIME_CHOICES.append(
-                    (str(j) + ":" + "00",str(j) + ":" + "00")
-                )
-            if j < 18:
-                TIME_CHOICES.append(
-                    (str(j) + ":" + "30",str(j) + ":" + "30")
-                )
-    # 30分後の分単位が30より大きい場合
-    if today_30.minute > 29:
-        for j in range(today_30.hour, 19):
-            if j != today_30.hour:
-                TIME_CHOICES.append(
-                    (str(j) + ":" + "00",str(j) + ":" + "00")
-                )
-            if j < 18:
-                TIME_CHOICES.append(
-                    (str(j) + ":" + "30",str(j) + ":" + "30")
-                )
-# 現在時刻が17:30以降の場合
+    # 本日から1週間分を取得
+    days = [date.today() + timedelta(days=day) for day in range(7)]
+    get_weeks("【本日】")
+# 現在時刻が17:30以降
 elif hour > 17 or ( hour == 17 and minute > 29 ):
-    for j in range(11, 19):
-        if j != 11:
+    # 翌日から1週間分を取得
+    days = [date.today() + timedelta(days=day+1) for day in range(7)]
+    get_weeks("【明日】")
+
+
+TIME_CHOICES = []
+def get_times(start_time):
+    for j in range(start_time, 19):
+        if j != start_time:
             TIME_CHOICES.append(
                 (str(j) + ":" + "00",str(j) + ":" + "00")
             )
@@ -86,12 +55,29 @@ elif hour > 17 or ( hour == 17 and minute > 29 ):
                 (str(j) + ":" + "30",str(j) + ":" + "30")
             )
 
+# 現在時刻が11:00～17:29
+if 11 < hour <  17 or ( hour == 17 and minute < 30 ):
+    # 現在時刻から30分後
+    today_30 = datetime.datetime.now() + datetime.timedelta(minutes=30)
+    # 30分後の分単位が30より小さい場合
+    if today_30.minute < 30:
+        get_times(today_30.hour)
+    # 30分後の分単位が30より大きい場合
+    if today_30.minute >= 30:
+        for j in range(today_30.hour+1, 19):
+            TIME_CHOICES.append(
+                (str(j) + ":" + "00",str(j) + ":" + "00")
+            )
+            if j < 18:
+                TIME_CHOICES.append(
+                    (str(j) + ":" + "30",str(j) + ":" + "30")
+                )
+        
+# 現在時刻が17:30～翌10:59
+elif ( hour == 17 and minute >= 30 ) or ( 18 < hour <= 24 ) or ( hour < 11 ):
+    get_times(11)
+
+
 class ReceiptForm(forms.Form):
-    date = forms.ChoiceField(
-        widget=forms.Select,
-        choices=DATE_CHOICES,
-    )
-    time = forms.ChoiceField(
-        widget=forms.Select,
-        choices=TIME_CHOICES,
-    )
+    date = forms.ChoiceField(widget=forms.Select, choices=DATE_CHOICES)
+    time = forms.ChoiceField(widget=forms.Select, choices=TIME_CHOICES)
