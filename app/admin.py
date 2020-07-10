5a7b3e7625cf0d1fa46f6339c1993ca4cb829c69
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-07-11 00:48:55 +0900

    モデル変更

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import OrderItem, Order, SizeItem, FlavorItem, OptionItem
+from .models import Order, Cart, SizeItem, FlavorItem, OptionItem
 
-admin.site.register(OrderItem)
 admin.site.register(Order)
+admin.site.register(Cart)
 admin.site.register(SizeItem)
 admin.site.register(FlavorItem)
 admin.site.register(OptionItem)
--- app/models.py
+++ app/models.py
@@ -3,51 +3,6 @@ from django.db import models
 from django.utils import timezone
 
 
-# class OrderHistory(models.Model):
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     size_title = models.CharField('サイズ', max_length=100)
-#     size_price = models.CharField('サイズ価格', max_length=100)
-#     flavor_title = models.CharField('フレーバー', max_length=100)
-#     flavor_title_2 = models.CharField('フレーバー2', max_length=100, blank=True, null=True)
-#     option_title = models.CharField('オプション', max_length=100, blank=True, null=True)
-#     option_price = models.CharField('オプション価格', max_length=100, blank=True, null=True)
-#     option_title_2 = models.CharField('オプション2', max_length=100, blank=True, null=True)
-#     option_price_2 = models.CharField('オプション2価格', max_length=100, blank=True, null=True)
-#     total_price = models.IntegerField('合計')
-#     created = models.DateTimeField('登録日', default=timezone.now)
-
-#     def __str__(self):
-#         return f'{self.size_title}　{self.created}'
-
-
-# class Order(models.Model):
-#     name = models.CharField('お名前', max_length=30)
-#     furigana = models.CharField('フリガナ', max_length=30)
-#     email = models.EmailField('メールアドレス', max_length=256)
-#     tel = models.CharField('電話番号', max_length=13)
-#     receipt = models.CharField('受取日時', max_length=30)
-#     created = models.DateTimeField('注文受付日', default=timezone.now)
-
-#     def __str__(self):
-#         return f'{self.name}　{self.created}'
-
-
-# class Cart(models.Model):
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     size_title = models.CharField('サイズ', max_length=100)
-#     size_price = models.CharField('サイズ価格', max_length=100)
-#     flavor_title = models.CharField('フレーバー', max_length=100)
-#     flavor_title_2 = models.CharField('フレーバー2', max_length=100, blank=True, null=True)
-#     option_title = models.CharField('オプション', max_length=100, blank=True, null=True)
-#     option_price = models.CharField('オプション価格', max_length=100, blank=True, null=True)
-#     option_title_2 = models.CharField('オプション2', max_length=100, blank=True, null=True)
-#     option_price_2 = models.CharField('オプション2価格', max_length=100, blank=True, null=True)
-#     total_price = models.IntegerField('合計')
-
-#     def __str__(self):
-#         return self.size_title
-
-
 class SizeItem(models.Model):
     title = models.CharField('サイズ', max_length=100)
     price = models.IntegerField('価格')
@@ -83,32 +38,36 @@ class OptionItem(models.Model):
         return self.title
 
 
-class OrderItem(models.Model):
+class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    ordered = models.BooleanField(default=False)
-    size_item = models.ForeignKey(SizeItem, on_delete=models.CASCADE)
-    flavor_item = models.ManyToManyField(FlavorItem)
-    option_item = models.ManyToManyField(OptionItem)
-
-    def get_total_item_price(self):
-        return self.size_item.price + self.flavor_item.price + self.option_item.price
+    ordered = models.BooleanField('注文完了',default=False)
+    start_date = models.DateTimeField('注文時刻', auto_now_add=True)
+    size_title = models.CharField('サイズ', max_length=100)
+    size_price = models.IntegerField('サイズ価格')
+    flavor_title = models.CharField('フレーバー', max_length=100)
+    flavor_price = models.IntegerField('フレーバー価格', default=0)
+    flavor2_title = models.CharField('フレーバー2', max_length=100, blank=True, null=True)
+    flavor2_price = models.IntegerField('フレーバー2価格', default=0)
+    option_title = models.CharField('オプション', max_length=100, blank=True, null=True)
+    option_price = models.IntegerField('オプション価格', default=0)
+    option2_title = models.CharField('オプション2', max_length=100, blank=True, null=True)
+    option2_price = models.IntegerField('オプション2価格', default=0)
+    option3_title = models.CharField('オプション3', max_length=100, blank=True, null=True)
+    option3_price = models.IntegerField('オプション3価格', default=0)
+    total_price = models.IntegerField('合計')
 
     def __str__(self):
-        return self.user.name
+        return f'{self.user.name}　{self.size_title}'
 
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    items = models.ManyToManyField(OrderItem)
-    start_date = models.DateTimeField(auto_now_add=True)
-    ordered_date = models.DateTimeField()
-    ordered = models.BooleanField(default=False)
-
-    def get_total(self):
-        total = 0
-        for order_item in self.items.all():
-            total += order_item.get_total_item_price()
-        return total
+    name = models.CharField('お名前', max_length=30)
+    furigana = models.CharField('フリガナ', max_length=30)
+    email = models.EmailField('メールアドレス', max_length=256)
+    tel = models.CharField('電話番号', max_length=13)
+    receipt = models.CharField('受取日時', max_length=30)
+    created = models.DateTimeField('注文受付日', default=timezone.now)
 
     def __str__(self):
-        return self.user.name
+        return f'{self.name}　{self.created}'
